924ff0b8385b3901f6f84e85f4633a533da8e8a8
Esmé Cowles <escowles@ticklefish.org>
2026-07-20 15:27:05 -0400

    fixing syntax issues

--- static.sh
+++ static.sh
@@ -1,42 +1,14 @@
 #!/bin/bash
 
-STUB_ADM=$( cat << END_ADM
-{
-  "admin_set":"",
-  "collections":[],
-  "label":"",
-  "rights":"https://rightsstatements.org/vocab/CNE/1.0/",
-  "source_metadata_id":"",
-  "visibility":"private"
-}
-END_ADM
-)
-
-echo "$STUB_ADM" | jq .
-
-# tech md
-# -> start with:
-# > identify channels.tif 
-# channels.tif TIFF 349x187 349x187+0+0 8-bit Grayscale Gray 4126B 0.000u 0:00.001
-# > stat channels.tif 
-# 16777231 76740006 -rwx------ 1 esc staff 0 4126 "Jul 17 07:43:28 2026" "May 10 09:15:44 1998" "Jul 17 07:43:28 2026" "May 10 09:15:44 1998" 4096 16 0x40 channels.tif
-# > ls -l channels.tif 
-# -rwx------  1 esc  staff  4126 May 10  1998 channels.tif
-# > sha256sum channels.tif 
-# 9c88e4874b1a75d4f41634a23f962700c02a27e009c2b6d60bc7312365bc1e35  channels.tif
-# > md5sum channels.tif 
-# 58d2beef945772e754d744a24fb53d41  channels.tif
-
-# deriv
-# convert foo.tif foo.ptiff
+# bash implementation of static repository
 
 ###############################################################################
 # config
 ###############################################################################
 
 # main location of files
-REPO_DIR="$HOME/Downloads/static_repo/repo"
-PUB_DIR="$HOME/Downloads/static_repo/public"
+REPO_DIR="$HOME/Downloads/static_repo/tmp/repo"
+PUB_DIR="$HOME/Downloads/static_repo/tmp/public"
 
 # derivative generation options
 IMG_MGK_OPTS="" # ZZZ maxsize 1024x1024?
@@ -113,7 +85,7 @@ function remote_metaata_type
   else
     LEN=$( echo "$ID" | wc -c )
     START=$( echo "$ID" | cut -c1-2 )
-    if [ $LEN -gt 9 && "$START" = "99" ]; then
+    if [ $LEN -gt 9 -a "$START" = "99" ]; then
       echo alma
     else
       echo none
@@ -130,9 +102,10 @@ function update_remote_metadata
   DESC_MD_FILE="$OBJ_DIR/_desc_md.json"
   if [ "$TYPE" = "alma" ]; then
     curl "${ALMA_MD_PREFIX}${ID}${ALMA_MD_SUFFIX}" > "$DESC_MD_FILE"
-  elsif [ "$TYPE" = "aspace" ]; then
+  elif [ "$TYPE" = "aspace" ]; then
     curl "${ASPACE_MD_PREFIX}${ID}${ASPACE_MD_SUFFIX}"> "$DESC_MD_FILE"
   else
+    echo "error"
     # ZZZ error
   fi
 }
@@ -164,7 +137,7 @@ function update_tech_md
     SZ=$( stat -f %z $i )
     MD5=$( md5 -q $i )
     SHA=$( sha256 -q $i )
-    echo "{\"filename\":\"$FN\",\"created\":\"$CT\",\"modified\":\"$MT\",\"size\":"$SZ\",\"md5\":\"$MD5\",\"sha256\":\"$SHA\"}" >> $TMD
+    echo "{\"filename\":\"$FN\",\"created\":\"$CT\",\"modified\":\"$MT\",\"size\":\"$SZ\",\"md5\":\"$MD5\",\"sha256\":\"$SHA\"}" >> $TMD
   done
   echo ']' > $TMD
 }
@@ -183,12 +156,12 @@ if [ "$1" = "object:create" ]; then
     cp -av $i $OBJ_DIR/
   done
   for f in $MD_FILES; do
-    if [ -f "$SRC_DIR/$f ]; then
+    if [ -f "$SRC_DIR/$f" ]; then
       cp -v "$SRC_DIR/$f" "$OBJ_DIR/$f"
     elif [ "$f" = "_desc_md.json" ]; then
       DIRNAME=$( dirname "$SRC_DIR" )
       ID_TYPE=$( remote_metadata_type "$DIRNAME" )
-      if [ "$ID_TYPE" = "alma" || "$ID_TYPE" = "aspace" ]; then
+      if [ "$ID_TYPE" = "alma" -o "$ID_TYPE" = "aspace" ]; then
         update_remote_metadata $UUID $ID_TYPE $DIRNAME
       else
         echo "{\"label\":\"$DIRNAME\"}" > $OBJ_DIR/_desc_md.json
@@ -197,6 +170,7 @@ if [ "$1" = "object:create" ]; then
       update_tech_md $UUID
     else
       # ZZZ stub _admin _order _struct
+      echo "XXX"
     fi
   done
   # ZZZ error if no files copied? or warn if only md files copied?
@@ -240,7 +214,7 @@ elif [ "$1" = "object:refresh_remote_md" ]; then
   UUID="$2"
   OBJ_DIR=$( obj_dir "$UUID" )
   ID_TYPE=$( jq -r .remote_metadata_type "$OBJ_DIR/_adm_md.json" )
-  if [ "$ID_TYPE" = "alma" || "$ID_TYPE" = "aspace" ]; then
+  if [ "$ID_TYPE" = "alma" -o "$ID_TYPE" = "aspace" ]; then
     ID=$( jq -r .remote_metadata_id "$OBJ_DIR/_adm_md.json" )
     update_remote_metadata $UUID $ID_TYPE $ID
   fi
@@ -248,10 +222,10 @@ elif [ "$1" = "object:derivatives" ]; then
   UUID="$2"
   object_derivatives "$UUID"
 elif [ "$1" = "object:manifest" ]; then
-	[uuid]
+  # [uuid]
   # XXX
 elif [ "$1" = "object:validate" ]; then
-	[uuid]
+  # [uuid]
   # XXX
 elif [ "$1" = "admin_set:create" ]; then
   UUID=$( mint_uuid )
@@ -265,8 +239,8 @@ elif [ "$1" = "admin_set:update" ]; then
   # ZZZ error if no JSON file
   cp -v "$JSON" "$ADMIN_SETS_DIR/$UUID.json"
 elif [ "$1" = "admin_set:manifest" ]; then
-	[uuid]
-  # XXX
+  # [uuid]
+  echo XXX
 elif [ "$1" = "collection:create" ]; then
   UUID=$( mint_uuid )
   JSON="$2"
@@ -279,10 +253,13 @@ elif [ "$1" = "collection:update" ]; then
   # ZZZ error if no JSON file
   cp -v "$JSON" "$COLLECTIONS_DIR/$UUID.json"
 elif [ "$1" = "collection:manifest" ]; then
-	[uuid]
+  # [uuid]
   # XXX
+  echo XXX
 elif [ "$1" = "sitemap" ]; then
   # XXX
+  echo XXX
 else
-	# ZZZ usage
+  # ZZZ usage
+  echo usage
 fi
